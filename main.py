b13ecf0286757da1d535dc94deaee1d6f5089ff4
nixaty <nixaty@nixaty.ru>
2025-08-29 19:36:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -11,10 +11,9 @@ import maxapi
 
 
 async def main():
-    ws = await maxapi.connect()
-    print(ws.id)
-    await maxapi.run()
+    asyncio.create_task(maxapi.run())
     await tgbot.bot.delete_webhook(drop_pending_updates=True)
+    print(1)
     await tgbot.dp.start_polling(tgbot.bot, allowed_updates=tgbot.dp.resolve_used_update_types())
 
 
