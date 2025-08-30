cefb47ec4e56c514ab9e1b7b47ae1dc122dffdd6
nixaty <nixaty@nixaty.ru>
2025-08-30 15:12:25 +0400

    Update all

--- maxapi.py
+++ maxapi.py
@@ -17,37 +17,116 @@ headers = {
     "Origin": "https://web.max.ru",
     "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
 }
-seq = 3
 
 
-async def names_by_ids(ws: websockets.ClientConnection, ids: list[int]):
-    global seq
-    await ws.send(json.dumps({
-        "ver": 11,
-        "cmd": 0,
-        "seq": seq,
-        "opcode": 32,
-        "payload": {
-            "contactIds": ids
+async def get_auth_requests():
+    return [
+        {
+            "userAgent":{
+                "deviceType":"WEB",
+                "locale":"ru",
+                "deviceLocale":"en",
+                "osVersion":"Windows",
+                "deviceName":"Chrome",
+                "headerUserAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
+                "appVersion":"25.8.10","screen":"1080x1920 1.0x",
+                "timezone":"Europe/Saratov"
+            },
+            "deviceId":getenv("MAX_DEVICE_ID")
+        },
+        {
+            "interactive":True,
+            "token":getenv("MAX_PAYLOAD_TOKEN"),
+            "chatsCount":40,
+            "chatsSync":0,
+            "contactsSync":0,
+            "presenceSync":0,
+            "draftsSync":0
         }
-    }))
-    response = json.loads(await ws.recv())
-    names = []
-    for user in response["payload"]["contacts"]:
-        fullname = user["names"][0]["firstName"] + user["names"][0]["lastName"]
-        names.append(fullname)
 
-    seq += 1
-
-    return names
+    ]
 
 
-async def receiver_calback(ws: websockets.ClientConnection, msg: dict):
+class MaxWSClient:
+    def __init__(self, url, additional_headers):
+        self.url = url
+        self.additional_headers = additional_headers
+        self.last_seq = -1
+        self.pending = {}
+        self.event_handlers = []
+
+    async def _connect(self):
+        while True:
+            self.ws = await websockets.connect(self.url, additional_headers=self.additional_headers)
+            await self._reader()
+            await self.ws.wait_closed()
+
+    async def start(self):
+        asyncio.create_task(self._connect())
+        await asyncio.sleep(1)
+
+    async def _reader(self):
+        try:
+            async for msg in self.ws:
+                data = json.loads(msg)
+
+                seq = data["seq"]
+                opcode = data["opcode"]
+
+                if seq is not None and seq > self.last_seq:
+                    self.last_seq = seq
+
+                if seq in self.pending:
+                    record = self.pending[seq]
+                    fut, expected_opcode = record["future"], record["opcode"]
+
+                    if opcode == expected_opcode:
+                        if not fut.done():
+                            fut.set_result(data)
+                        self.pending.pop(seq)
+                        continue
+
+                for handler in self.event_handlers:
+                    asyncio.create_task(handler(self, data))
+
+        except websockets.ConnectionClosed:
+            logger.info("Connection closed by server. Reconnecting")
+
+
+    async def send_request(self, opcode, payload):
+        self.last_seq += 1
+        seq = self.last_seq
+
+        packet = {
+            "ver": 11,
+            "cmd": 0,
+            "seq": seq,
+            "opcode": opcode,
+            "payload": payload,
+        }
+
+        fut = asyncio.get_event_loop().create_future()
+        self.pending[seq] = {"future": fut, "opcode": opcode}
+
+        await self.ws.send(json.dumps(packet))
+        return await fut
+
+
+    def on_event(self, callback):
+        self.event_handlers.append(callback)
+
+
+async def forward_message(client: MaxWSClient, msg: dict):
     if str(msg["payload"]["chatId"]) not in str(getenv("ALLOWED_MAX_CHATS")):
         return
-    attaches = msg["payload"]["message"]["attaches"]
+
+    uid = msg["payload"]["message"]["sender"]
+    contacts = (await client.send_request(32, {"contactIds":[uid]}))["payload"]["contacts"]
+    fullname = contacts[0]["names"][0]["firstName"] + " " + contacts[0]["names"][0]["lastName"]
+
     text = msg["payload"]["message"]["text"]
-    fullname = (await names_by_ids(ws, [msg["payload"]["message"]["sender"]]))[0]
+    attaches = msg["payload"]["message"]["attaches"]
+    
     # if attaches:
     #     attaches_urls = [attach["baseUrl"] for attach in attaches]
     #     print(attaches_urls)
@@ -59,71 +138,24 @@ async def receiver_calback(ws: websockets.ClientConnection, msg: dict):
         )
 
 
-async def message_receiver(ws: websockets.ClientConnection, callback):
-    try:
-        async for msg in ws:
-            msgjson = json.loads(msg)
-            if msgjson["opcode"] == 128:
-                await callback(ws, msgjson)
-                
-    except websockets.ConnectionClosed:
-        logger.info("Connection closed by server")
+async def on_update(client: MaxWSClient, msg: dict):
+    payload = msg["payload"]
+
+    if msg["opcode"] == 128:
+        await forward_message(client, msg)
 
 
-async def start_receiver(ws: websockets.ClientConnection, callback):
-    asyncio.create_task(message_receiver(ws, callback))
+async def start():
+    max = MaxWSClient(url, headers)
 
+    await max.start()
 
-async def send_auth(ws: websockets.ClientConnection):
-    await ws.send(json.dumps(
-        {
-            "ver":11,
-            "cmd":0,
-            "seq":0,
-            "opcode":6,
-            "payload":{
-                "userAgent":{
-                    "deviceType":"WEB",
-                    "locale":"ru",
-                    "deviceLocale":"en",
-                    "osVersion":"Windows",
-                    "deviceName":"Chrome",
-                    "headerUserAgent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36",
-                    "appVersion":"25.8.10","screen":"1080x1920 1.0x",
-                    "timezone":"Europe/Saratov"
-                },
-                "deviceId":getenv("MAX_DEVICE_ID")
-            }
-        }
-    ))
+    # Send auth packets
+    auth_packets = await get_auth_requests()
+    await max.send_request(6, auth_packets[0])
+    await max.send_request(19, auth_packets[1])
 
-    await ws.send(json.dumps(
-        {
-            "ver":11,
-            "cmd":0,
-            "seq":2,
-            "opcode":19,
-            "payload": {
-                "interactive":True,
-                "token":getenv("MAX_PAYLOAD_TOKEN"),
-                "chatsCount":40,
-                "chatsSync":0,
-                "contactsSync":0,
-                "presenceSync":0,
-                "draftsSync":0
-            }
-        }
-    ))
+    # Register events
+    max.on_event(on_update)
 
-
-async def connect():
-    ws = await websockets.connect(url, additional_headers=headers)
-    return ws
-
-
-async def run():
-    while True:
-        ws = await connect()
-        await send_auth(ws)
-        await start_receiver(ws, receiver_calback)
-        await ws.wait_closed()
+    logger.info("MAX bot started")
